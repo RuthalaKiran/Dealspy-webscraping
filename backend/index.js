a7b8eeb9ff7dced264702d5468e2c3b38cd782ab
Ruthala kiran <kiranruthalakiran@gmail.com>
2024-07-30 17:03:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -55,10 +55,10 @@ cron.schedule('0 0 * * 0', async () => {
 });
 
 // confronting the 404 not found error
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(PORT, () => {
