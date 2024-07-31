27a8066afd27c08cdce109920da2c5f59b9f9a18
Ruthala kiran <kiranruthalakiran@gmail.com>
2024-07-31 11:47:20 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="bg-gray-800 flex flex-col text-center px-5 py-10 ">
       <div className="flex items-center justify-center gap-2">
-        <img src={logo} className="w-10 mb-3" alt="" />
+       <Link to="/"   > <img src={logo} className="w-10 mb-3 cursor-pointer" alt="" /></Link>
         <p className="text-2xl pb-4 flex items-center justify-center text-gray-200 font-bold">
           DeaL <span className="text-red-700">Spy</span>
         </p>
